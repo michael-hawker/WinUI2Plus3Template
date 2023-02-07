032308842ec345c491fdebd5f3f25c5f6aae25bb
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2023-02-07 15:18:48 -0800

    Flatten Root Namespace and Share App.xaml file
    Install WinUI 2 in UWP head
    Update Windows App SDK to latest 1.2 stable

--- ProjectTemplate.WinAppSDK/App.xaml.cs
+++ ProjectTemplate.WinAppSDK/App.xaml.cs
@@ -4,7 +4,7 @@ using Microsoft.UI.Xaml;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace ProjectTemplate.WinAppSDK;
+namespace ProjectTemplate;
 
 /// <summary>
 /// Provides application-specific behavior to supplement the default Application class.
--- ProjectTemplate.WinAppSDK/MainWindow.xaml.cs
+++ ProjectTemplate.WinAppSDK/MainWindow.xaml.cs
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft Corporation and Contributors.
-// Licensed under the MIT License.
-
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -19,21 +16,20 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace ProjectTemplate.WinAppSDK
-{
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            this.InitializeComponent();
-        }
+namespace ProjectTemplate;
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-        }
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
+    }
+
+    private void myButton_Click(object sender, RoutedEventArgs e)
+    {
+        myButton.Content = "Clicked";
     }
 }
