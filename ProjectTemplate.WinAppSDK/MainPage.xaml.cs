0e28f13e88e83150e795da96fc8e1543bbb00188
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2023-02-07 15:30:33 -0800

    Add MainPage.xaml example
    Add to WindowsAppSDK head to reference in MainWindow
    Reference in UWP head (removing existing MainPage.xaml)

---
+++ ProjectTemplate.WinAppSDK/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+#if WINDOWS_UWP
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+#else
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+#endif
+
+namespace ProjectTemplate;
+
+public sealed partial class MainPage : Page
+{
+    public MainPage()
+    {
+        this.InitializeComponent();
+    }
+}
